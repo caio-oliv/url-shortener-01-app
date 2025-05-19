46c9d9d83f6e28422fef4bf6f03b6db3818bafa2
Caio Oliveira <caio.vsoliveira23@gmail.com>
2025-05-19 14:31:41 -0300

    feat: project setup

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+/// <reference types="vitest" />
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  appType: 'spa',
+	envPrefix: 'APP_',
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	server: {
+		port: 4000,
+		strictPort: true,
+		host: '::1',
+		open: false,
+	},
+	preview: {
+		port: 5000,
+		host: '::1',
+		open: false,
+	},
+	test: {
+		environment: 'node',
+	},
+})
